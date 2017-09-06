264a365a4447f11a10685dd74bc75287e52da939
Adrian DC <radian.dc@gmail.com>
2017-09-05 20:03:00 -0400

    g3: camera: Use wrapper parameters instead of CameraParameters
     * Get inline with the lack of CameraParametersExtra.h
        support in AOSP, avoid using device header to inject
        additional parameters, especially with the introduction
        of Android.bp compilation for libcamera_client
        which ignores the device headers override
    
    Change-Id: I754116f55ed32d54d1b1ad44407993137a07fb69

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012-2015, The CyanogenMod Project
+ * Copyright (C) 2017 - The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +34,31 @@
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
 #include <camera/CameraParametersExtra.h>
+
+static const char AUDIO_ZOOM_OFF[] = "audio-zoom";
+static const char AUDIO_ZOOM_ON[] = "audio-zoom";
+static const char BEAUTY_SHOT_OFF[] = "beauty-shot";
+static const char BEAUTY_SHOT_ON[] = "beauty-shot";
+static const char BURST_SHOT_OFF[] = "burst-shot";
+static const char BURST_SHOT_ON[] = "burst-shot";
+static const char KEY_AUDIO_ZOOM[] = "audio-zoom";
+static const char KEY_AUDIO_ZOOM_SUPPORTED[] = "audio-zoom-supported";
+static const char KEY_BEAUTY_SHOT[] = "beauty-shot";
+static const char KEY_BEAUTY_SHOT_SUPPORTED[] = "beauty-shot-supported";
+static const char KEY_BURST_SHOT[] = "burst-shot";
+static const char KEY_BURST_SHOT_SUPPORTED[] = "burst-shot-supported";
+static const char KEY_FOCUS_MODE_OBJECT_TRACKING[] = "object-tracking";
+static const char KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[] = "object-tracking-supported";
+static const char KEY_ISO_MODE[] = "iso";
+static const char KEY_LGE_CAMERA[] = "lge-camera";
+static const char KEY_LGE_ISO_MODE[] = "lg-iso";
+static const char KEY_SUPPORTED_ISO_MODES[] = "iso-values";
+static const char KEY_VIDEO_WDR[] = "video-wdr";
+static const char KEY_VIDEO_WDR_SUPPORTED[] = "video-wdr-supported";
+static const char VIDEO_WDR_OFF[] = "video-wdr";
+static const char VIDEO_WDR_ON[] = "video-wdr";
+static const char OBJECT_TRACKING_ON[] = "object-tracking";
+static const char OBJECT_TRACKING_OFF[] = "object-tracking";
 
 static const char AUDIO_ZOOM_OFF[] = "audio-zoom";
 static const char AUDIO_ZOOM_ON[] = "audio-zoom";
