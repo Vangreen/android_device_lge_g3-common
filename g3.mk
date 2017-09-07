042f0b7983745e049bc8804a1b2aaf09ae278066
Vangreen <pwangryn@op.pl>
2017-09-07 23:28:32 +0200

    Revert "g3: Build HIDL Hals"
    This reverts commit 888b058d55d3c63a4a3c75b07f43b377499de8d1.

--- g3.mk
+++ g3.mk
@@ -70,8 +70,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
     audiod \
     audio.primary.msm8974 \
     audio_policy.msm8974 \
@@ -89,8 +87,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+<<<<<<< HEAD
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor \
+=======
+>>>>>>> parent of 888b058... g3: Build HIDL Hals
     hwaddrs
 
 # Camera
@@ -100,19 +101,11 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.memtrack@1.0-impl \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
     memtrack.msm8974 \
     liboverlay
-
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
@@ -127,18 +120,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     keystore.msm8974
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
-
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
     lights.msm8974
 
 # Location
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
     gps.msm8974
 
 PRODUCT_COPY_FILES += \
@@ -156,7 +143,6 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
     com.android.nfc_extras \
     Tag
 
@@ -182,7 +168,6 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
     power.msm8974
 
 # Radio
@@ -203,22 +188,17 @@ PRODUCT_PACKAGES += \
     init.sensors.sh \
     ueventd.g3.rc
 
-# Sensors
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
-
 # Wifi
 PRODUCT_PACKAGES += \
+<<<<<<< HEAD
     android.hardware.wifi@1.0-service \
     wificond \
+=======
+>>>>>>> parent of 888b058... g3: Build HIDL Hals
     hostapd \
     hostapd_default.conf \
     libwpa_client \
